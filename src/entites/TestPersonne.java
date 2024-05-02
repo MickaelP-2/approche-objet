480d1375773a8d8870935f5f899174440a55f96d
Penon Mickael <penon.mickael@orange.fr>
2024-05-02 11:55:05 +0200

    commit du 02/05/2024(2)

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -1,6 +1,6 @@
 package entites;
 
-
+import entites2.Personne;
 public class TestPersonne {
 
 	public static void main(String[] args) {
@@ -8,12 +8,20 @@ public class TestPersonne {
 		Personne pers1 = new Personne();
 		pers1.nom = "Bon";
 		pers1.prenom = "Jean";
-		pers1.adressePostale = "2 rue Charcutt";
+		pers1.adressePostale = new AdressePostale();
+		pers1.adressePostale.numeroDeRue = 11;
+		pers1.adressePostale.libelleDeRue = " rue des champs";
+		pers1.adressePostale.numeroDeRue = 34000;
+		pers1.adressePostale.ville = "Montpellier";
 		
 		Personne pers2 = new Personne();
 		pers2.nom = "Bonot";
 		pers2.prenom = "Jean";
-		pers2.adressePostale = "3 avenue de l'abbé Cochon";
+		pers2.adressePostale = new AdressePostale();
+		pers2.adressePostale.numeroDeRue = 31;
+		pers2.adressePostale.libelleDeRue = " impasse du fond";
+		pers2.adressePostale.numeroDeRue = 30000;
+		pers2.adressePostale.ville = "Nimes";
 	}
 
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -1,10 +1,12 @@
 package entites2;
 
+import entites.AdressePostale;
+
 public class Personne {
 
 		public String nom;
 		public String prenom;
-		public String adressePostale;
+		public AdressePostale adressePostale;
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
